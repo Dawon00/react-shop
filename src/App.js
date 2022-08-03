40e2f568a5328490d03520bbd3912df6817a9540
Dawon00 <da6921@naver.com>
2022-08-04 02:51:34 +0900

    add image, public folder

--- src/App.js
+++ src/App.js
@@ -1,8 +1,51 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { Button, Container, Nav, Navbar, Row, Col } from "react-bootstrap";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Shop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+
+      <div className="main-bg"></div>
+
+      <div>
+        <Row>
+          <Col sm>
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            ></img>
+            <h4>상품명</h4>
+            <p>상품 소개</p>
+          </Col>
+          <Col sm>
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            ></img>
+            <h4>상품명</h4>
+            <p>상품 소개</p>
+          </Col>
+          <Col sm>
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            ></img>
+            <h4>상품명</h4>
+            <p>상품 소개</p>
+          </Col>
+        </Row>
+      </div>
+    </div>
+  );
 }
 
 export default App;
