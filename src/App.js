8f0fc63be8988ad61ca1b6b35af0f2ee8d1ad1a7
Dawon00 <da6921@naver.com>
2022-09-04 21:08:24 +0900

    lazy import

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import "./App.css";
-import { useEffect, useState } from "react";
+import { lazy, Suspense, useEffect, useState } from "react";
 import { Button, Container, Nav, Navbar, Row } from "react-bootstrap";
 import data from "./data.js";
-import Detail from "./routes/Detail.js";
 import { Routes, Route, useNavigate, Outlet } from "react-router-dom";
 import axios from "axios";
-import Cart from "./routes/Cart.js";
 import { useQuery } from "react-query";
+
+// import Detail from "./routes/Detail.js";
+// import Cart from "./routes/Cart.js";
+
+const Detail = lazy(() => import("./routes/Detail.js"));
+const Cart = lazy(() => import("./routes/Cart.js"));
 
 function App() {
   useEffect(() => {
@@ -78,54 +82,59 @@ function App() {
         </Container>
       </Navbar>
 
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <div>
-                <div className="main-bg"></div>
+      <Suspense fallback={<div>loading...</div>}>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <div>
+                  <div className="main-bg"></div>
 
-                <Container>
-                  <Row>
-                    <Card shoes={shoes} navigate={navigate}></Card>
-                  </Row>
-                </Container>
-              </div>
-              <button
-                onClick={() => {
-                  axios
-                    .get("https://codingapple1.github.io/shop/data2.json")
-                    .then((x) => {
-                      console.log(x.data);
-                      setShoes(shoes.concat(x.data));
-                      //let copy = [...shoes, ...x.data];
-                      //setShoes(copy);
-                    })
-                    .catch(() => {
-                      console.log("fail");
-                    });
-                }}
-              >
-                버튼
-              </button>
-            </>
-          }
-        ></Route>
-        <Route path="/detail/:id" element={<Detail shoes={shoes} />}></Route>
-        <Route path="/about" element={<About />}>
-          <Route path="member" element={<Member />}></Route>
-          <Route path="location" element={<Location />}></Route>
-        </Route>
+                  <Container>
+                    <Row>
+                      <Card shoes={shoes} navigate={navigate}></Card>
+                    </Row>
+                  </Container>
+                </div>
+                <button
+                  onClick={() => {
+                    axios
+                      .get("https://codingapple1.github.io/shop/data2.json")
+                      .then((x) => {
+                        console.log(x.data);
+                        setShoes(shoes.concat(x.data));
+                        //let copy = [...shoes, ...x.data];
+                        //setShoes(copy);
+                      })
+                      .catch(() => {
+                        console.log("fail");
+                      });
+                  }}
+                >
+                  버튼
+                </button>
+              </>
+            }
+          ></Route>
+          <Route path="/detail/:id" element={<Detail shoes={shoes} />}></Route>
+          <Route path="/about" element={<About />}>
+            <Route path="member" element={<Member />}></Route>
+            <Route path="location" element={<Location />}></Route>
+          </Route>
 
-        <Route path="/event" element={<EventPage />}>
-          <Route path="one" element={<p>첫 주문시 양배추즙 서비스</p>}></Route>
-          <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
-        </Route>
-        <Route path="/cart" element={<Cart />} />
+          <Route path="/event" element={<EventPage />}>
+            <Route
+              path="one"
+              element={<p>첫 주문시 양배추즙 서비스</p>}
+            ></Route>
+            <Route path="two" element={<p>생일기념 쿠폰받기</p>}></Route>
+          </Route>
+          <Route path="/cart" element={<Cart />} />
 
-        <Route path="*" element={<div>404</div>}></Route>
-      </Routes>
+          <Route path="*" element={<div>404</div>}></Route>
+        </Routes>
+      </Suspense>
     </div>
   );
 }
